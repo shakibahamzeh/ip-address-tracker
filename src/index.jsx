89ed00adcdebd6e78ca0bbabfb3999998951255f
shakiba <shakiba.hamze70@gmail.com>
2024-07-02 15:21:28 +0330

    update index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,16 @@
 import { render } from 'preact';
+import IPAddressTracker from './app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import preactLogo from './assets/preact.svg';
-import './style.css';
-import Input from './components/input/input';
-import { ArrowRight2 } from 'iconsax-react';
-import Card from './components/card/card';
 
 export function App() {
+
+	const queryClient = new QueryClient() 
 	return (
 		<>
-		 <header>
-			<h1>IP Address Tracker</h1>
-			<div className="form-control">
-				<Input type="text" placeholder="Search for any IP address or domain" />
-				<div className="icon-wrapper">
-				 <ArrowRight2 size="20" />
-				</div>
-			</div>
-		 </header>
-		 <section>
-			<Card title={111} content={1111}/>
-			<Card title={111} content={1111}/>
-			<Card title={111} content={1111}/>
-			<Card title={111} content={1111}/>
-		 </section>
+		 <QueryClientProvider client={queryClient}>
+		 <IPAddressTracker/>
+		 </QueryClientProvider>
 		</>
 	);
 }
